cce347d8684e5683a0b65b9762f57e34ae7db5fc
Raihan Akram <raihanakram5611@gmail.com>
2023-06-20 18:01:29 +0300

    Timer
    A simple timer using Python.
    Takes input in seconds and shows the timer in hh:mm:ss format.

---
+++ timer.py
@@ -0,0 +1,18 @@
+import time
+
+# countdown function
+def countdown(t):
+    while t:
+        hours, remainder = divmod(t, 3600) #calculates number of hours and the remaining seconds by dividing time entered by 3600.
+        mins, secs = divmod(remainder, 60) #calculates number of minutes from the seconds left after calculating the hours. And gives the number of seconds left.
+        timer = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs) 
+        print(timer, end="\r")  # end = "\r" used to output the time on the same line after updating it
+        time.sleep(1)  # delays the running of code by 1 second
+        t -= 1
+
+    print('Time up !')
+
+t = input("\nPlease enter the time in seconds: ")
+
+countdown(int(t))
+
